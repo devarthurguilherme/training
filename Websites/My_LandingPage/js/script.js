c6ea97b68b83919f83765c17031c73fd696058c3
unknown <arthurguilherme2013@gmail.com>
2022-11-08 06:53:04 +0000

    Configuração da Navegação: Css e Js

--- Websites/My_LandingPage/js/script.js
+++ Websites/My_LandingPage/js/script.js
@@ -1,4 +1,6 @@
-function myFunction(){
-    let element = document.querySelector("#myDiv");
-    element.classList.toggle("myStyle");
+function toggle(){
+    let banner = document.querySelector("#banner");
+    let nav = document.querySelector("nav");
+    banner.classList.toggle("active");
+    nav.classList.toggle("active");
 };
