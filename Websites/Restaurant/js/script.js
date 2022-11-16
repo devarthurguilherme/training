4128f89a647de6dd4e35d37a883be95d598ea782
unknown <arthurguilherme2013@gmail.com>
2022-11-16 12:05:01 +0000

    Design Responsivo - Header

--- Websites/Restaurant/js/script.js
+++ Websites/Restaurant/js/script.js
@@ -4,3 +4,8 @@ function scrollMenu() {
     const header = document.querySelector("header");
     header.classList.toggle('sticky', window.scrollY > 0);
 }
+
+function toggleMenu() {
+    const menuToggle = document.querySelector(".menuToggle");
+    menuToggle.classList.toggle("active");
+}
