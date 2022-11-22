ff213e27379f20757c8d5ddc158d68adcc12a779
unknown <arthurguilherme2013@gmail.com>
2022-11-22 07:37:56 +0000

    Criação das seguintes áreas da Main Page ArthurGuilherme.com: about me, projects and knowledges

--- Portfolio_ArthurGuilherme/js/script.js
+++ Portfolio_ArthurGuilherme/js/script.js
@@ -1,13 +1,39 @@
-window.addEventListener('scroll', scrollMenu);
+/* Side Navigation */
+let arrow = document.querySelector(".fa-solid fa-arrow-right");
+let container = document.querySelector(".container");
 
-function scrollMenu() {
-    const header = document.querySelector("header");
-    header.classList.toggle('sticky', window.scrollY > 0);
-}
+function arrowBtn() {
 
-function toggleMenu() {
-    const menuToggle = document.querySelector(".menuToggle");
-    const navigation = document.querySelector("#navigation")
-    menuToggle.classList.toggle("active");
-    navigation.classList.toggle("active");
+    if(!container.classList.contains("hidden")) {
+        container.classList.add("hidden");
+    } else {
+        container.classList.remove("hidden");
+    };
+
+};
+/* End of Side Navigation */
+/* Gallery */
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".itemBox");
+
+for(let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function() {
+        for(let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let k = 0; k < itemBox.length; k++) {
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+
+            if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+                itemBox[k].classList.remove('hide');
+                itemBox[k].classList.add('active');
+            }
+        }
+    })
 }
+/* End of Gallery */
