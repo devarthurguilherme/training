b7dcd7299988e9a6cb8847304026f9b0f860e9f3
unknown <arthurguilherme2013@gmail.com>
2022-12-26 16:46:11 +0000

    Correction: My_Website and Creation: Websites/Travel

---
+++ My_Website_ArthurGuilhermeCom/projects/My_LandingPage/js/script.js
@@ -0,0 +1,6 @@
+function toggle(){
+    let banner = document.querySelector("#banner");
+    let nav = document.querySelector("nav");
+    banner.classList.toggle("active");
+    nav.classList.toggle("active");
+};
