6c8c4dc726102d540a832ae85be79a3579553315
unknown <arthurguilherme2013@gmail.com>
2023-01-05 13:01:58 +0000

    Criation of Section - Pack Travel

--- Websites/Travel_Project/js/script.js
+++ Websites/Travel_Project/js/script.js
@@ -4,3 +4,18 @@ function scrollMenu() {
     const header = document.querySelector("header").classList;
     header.toggle('scrollEffect', window.scrollY > 0)
 }
+
+//Accordion
+let accordion = document.querySelectorAll(".accordion");
+for(let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function (){
+        this.classList.toggle("active");
+
+        let painel = this.nextElementSibling;
+        if(painel.style.display === "block") {
+            painel.style.display = "none";
+        } else {
+            painel.style.display = "block";
+        }
+    })
+}
